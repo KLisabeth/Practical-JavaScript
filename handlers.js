2dbb54a972772c0d8da21f5b774179684db6db45
KLisabeth <professional.translations4ppl@gmail.com>
2020-03-31 16:42:13 +0200

    handlers.js

---
+++ handlers.js
@@ -0,0 +1,31 @@
+var handlers = {
+
+    addTodo: function () {
+        var addTodoTextInput = document.getElementById('addTodoTextInput');
+        todoList.addTodo(addTodoTextInput.value);
+        addTodoTextInput.value = '';
+        view.displayTodos();
+    },
+    changeTodo: function () {
+        var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+        var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+        todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+        changeTodoPositionInput.value = '';
+        changeTodoTextInput.value = '';
+        view.displayTodos();
+    },
+    deleteTodo: function (position) {
+       todoList.deleteTodo(position);
+        view.displayTodos();
+    },
+    toggleCompleted: function () {
+       var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        toggleCompletedPositionInput.value = '';
+        view.displayTodos();
+    },
+    toggleAll: function () {
+        todoList.toggleAll();
+        view.displayTodos();
+    }
+};
